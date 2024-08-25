b9421337cc113d46b524ee363883f6b6b5923b6e
rip <harwester1337@gmail.com>
2024-08-25 23:03:45 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import WelcomeBar from "./components/WelcomeBar";
+import Counter from "./components/Counter";
+import PostList from "./components/PostList";
+import './styles/App.css'
+import {useState} from "react";
+//import ClassCounter from "./components/ClassCounter";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {
+            id:1,
+            header: 'dark-crater 89',
+            body: 'You did good, slick'
+        },
+        {
+            id:2,
+            header: 'Reaper of Coal',
+            body: 'Rights of Wetto'
+        }
+    ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <WelcomeBar/>
+        <Counter/>
+        <form>
+            <input type="text" placeholder='Название'/>
+            <input type="text" placeholder='Описание'/>
+            <button>Создать пост</button>
+        </form>
+        <PostList posts={posts} title={'Список постов'}/>
     </div>
   );
 }
