08d5f8593ead810623bd9263cbd8f7d0aa377a5c
rip <harwester1337@gmail.com>
2024-08-25 17:51:18 +0700

    new shit

---
+++ src/components/UI/button/MyButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyButton = (props) => {
+    return (
+        <button>
+
+        </button>
+    );
+};
+
+export default MyButton;
---
+++ src/components/WelcomeBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomeBar = () => {
+    return (
+        <div>
+            <h1>Hello %username%, Welcome to my reactive krafted world!!</h1>
+        </div>
+    );
+};
+
+export default WelcomeBar;
